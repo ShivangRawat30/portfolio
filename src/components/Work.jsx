bde203f3c8cedb87c0601a4690b35a08d7163600
Shivang Rawat <rawatshivang30@.com>
2023-04-08 03:55:30 +0530

    c

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ImgResizer from "../assets/projects/ImageResizer.png";
-import Notes from "../assets/projects/Notes.png";
+import Finance from "../assets/projects/finance.png";
 import Translator from "../assets/projects/Translator.png";
 import Scramble from "../assets/projects/Scramble.png";
 import Piano from "../assets/projects/Piano.png";
@@ -20,6 +20,27 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
+            style={{ backgroundImage: `url(${Finance})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl text-white tracking-wider">
+                React Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://newapp-nu6c-bkhxw3yvu-shivangrawat30.vercel.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2  text-gray-700 font-bold text-2xl">
+                    Visit
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
             style={{ backgroundImage: `url(${ImgResizer})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -30,27 +51,6 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center">
                 <a href="https://resize-image-one.vercel.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2  text-gray-700 font-bold text-2xl">
-                    Visit
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${Notes})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl text-white tracking-wider">
-                JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://notes-app-two-drab.vercel.app/"
-                  target="_blank"
-                >
                   <button className="text-center rounded-lg px-4 py-3 m-2  text-gray-700 font-bold text-2xl">
                     Visit
                   </button>
